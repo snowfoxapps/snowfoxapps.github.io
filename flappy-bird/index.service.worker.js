fe5a0b59a86d5ba0b5be977007e600a16a36770e
evertonbaima <everton-baima@hotmail.com>
2025-04-30 20:56:04 -0300

    fix: flappy bird render

--- flappy-bird/index.service.worker.js
+++ flappy-bird/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745671261|2025219601';
+const CACHE_VERSION = '1746057269|483737494';
 /** @type {string} */
 const CACHE_PREFIX = 'Flappy Bird-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
